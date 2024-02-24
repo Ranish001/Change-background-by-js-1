c6def3feaa0ab60411d49517f1d5a4280796e00c
Ranish001 <157970811+Ranish001@users.noreply.github.com>
2024-02-24 08:27:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const red = () =>{
+  document.body.style.backgroundColor = "red";
+}
+const blue = () =>{
+  document.body.style.backgroundColor = "blue";
+}
+const green = () =>{
+  document.body.style.backgroundColor = "green";
+}
+const yellow = () =>{
+  document.body.style.backgroundColor = "yellow";
+}
+const black = () =>{
+  document.body.style.backgroundColor = "black";
+}
+const white = () =>{
+  document.body.style.backgroundColor = "white"
+}
